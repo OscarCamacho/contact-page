b3640b772b9dfad67b20dde8edb74ecd3d415a08
OscarCamacho <oscar@DESKTOP-AHHM0S1.localdomain>
2020-08-03 21:41:31 -0500

    dist folder delte. Angular json updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,32 @@
+import {
+    faHome,
+    faCalendarCheck,
+    faChartLine,
+    faEnvelopeOpen,
+} from '@fortawesome/free-solid-svg-icons';
+
 export const environment = {
-  production: true
+    production: true,
+    menuItems: [
+        {
+            name: 'Home',
+            routeLink: '/homescreen',
+            icon: faHome,
+        },
+        {
+            name: 'Professional Experience',
+            routeLink: '/xp',
+            icon: faCalendarCheck,
+        },
+        {
+            name: 'Skillset',
+            routeLink: '/skills',
+            icon: faChartLine,
+        },
+        {
+            name: 'Contact me!',
+            routeLink: '/contact',
+            icon: faEnvelopeOpen,
+        },
+    ],
 };
