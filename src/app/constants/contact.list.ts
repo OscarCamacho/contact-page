2def05a8e1db9eb4255fe71f57c731e9b3c04412
Oscar U. Camacho Hernandez <oscar.u.camacho.hernandez@gmail.com>
2022-01-27 11:40:09 -0600

    Added cv download links!

--- src/app/constants/contact.list.ts
+++ src/app/constants/contact.list.ts
@@ -1,5 +1,5 @@
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faEnvelope, faFile } from "@fortawesome/free-regular-svg-icons";
 import { ContactInfo } from "../model/contact.info";
 
 export const CONTACT_LIST: ContactInfo[] = [
@@ -17,5 +17,15 @@ export const CONTACT_LIST: ContactInfo[] = [
     name: "Mail",
     url: "mailto:oscar.u.camacho.hernandez@gmail.com",
     icon: faEnvelope
+  },
+  {
+    name: "Get my downloadable CV in English!",
+    url: "https://drive.google.com/file/d/1RZ-PFkzu4XadYIJlIRPHvY8ghirkEA8O/view?usp=sharing",
+    icon: faFile
+  },
+  {
+    name: "Get my downloadable CV in Spanish!",
+    url: "https://drive.google.com/file/d/16q0KZLvvSp9Aw_eNbUUpLobOlLYWGu-6/view?usp=sharing",
+    icon: faFile
   }
 ];
